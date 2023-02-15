086ba25050e4130a705a2b7ac51191b0219f2d01
benitdev <neihtben@gmail.com>
2023-02-15 19:35:07 +0700

    fix

--- dist/server.js
+++ dist/server.js
@@ -5,7 +5,6 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const cookie_parser_1 = __importDefault(require("cookie-parser"));
 const morgan_1 = __importDefault(require("morgan"));
-const path_1 = __importDefault(require("path"));
 const helmet_1 = __importDefault(require("helmet"));
 const express_1 = __importDefault(require("express"));
 require("express-async-errors");
@@ -40,7 +39,6 @@ app.use((0, cors_1.default)());
 app.use(express_1.default.json());
 app.use(express_1.default.urlencoded({ extended: true }));
 app.use((0, cookie_parser_1.default)(EnvVars_1.default.cookieProps.secret));
-app.use(express_1.default.static(path_1.default.join(__dirname, "public")));
 if (EnvVars_1.default.nodeEnv === enums_1.NodeEnvs.Dev) {
     app.use((0, morgan_1.default)("dev"));
 }
