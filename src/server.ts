47c2584578637a18d75fc27dd77dcb4867149216
benitdev <neihtben@gmail.com>
2023-02-15 19:33:25 +0700

    fix

--- src/server.ts
+++ src/server.ts
@@ -56,7 +56,7 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cookieParser(EnvVars.cookieProps.secret))
-app.use(express.static(path.join(__dirname, "public")))
+// app.use(express.static(path.join(__dirname, "public")))
 
 // Show routes called in console during development
 if (EnvVars.nodeEnv === NodeEnvs.Dev) {
